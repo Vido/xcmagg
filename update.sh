be9e1099d2507cf09333f2e73eeb17f4fe1511d7
Lucas Vido <vidov8@gmail.com>
2026-04-11 00:06:15 -0300

    UI Update

--- update.sh
+++ update.sh
@@ -9,3 +9,7 @@ sed -i "s|<lastmod>.*</lastmod>|<lastmod>${TODAY}</lastmod>|g" public/sitemap.xm
 
 scp public/* root@164.92.148.125:/var/www/xcmagg/
 
+sed "s|fetch('data.jsonl')|fetch('https://racefeed.com.br/data.jsonl')|" public/index.html > /tmp/index_prod.html
+scp /tmp/index_prod.html root@164.92.148.125:/var/www/xcmagg/index.html
+rm /tmp/index_prod.html
+
