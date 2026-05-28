dcda25532e7e1122939890be87fb172a45eac08a
Lucas Vido <vidov8@gmail.com>
2026-05-28 13:41:50 -0300

    Deploy

--- update.sh
+++ update.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
 source .env
-uv run main.py
-cp data/gold/data.jsonl public/data.jsonl
 
-TODAY=$(date -u +%Y-%m-%d)
-sed -i "s|<lastmod>.*</lastmod>|<lastmod>${TODAY}</lastmod>|g" public/sitemap.xml
+deploy() {
+    cp data/gold/data.jsonl public/data.jsonl
 
-scp public/* root@164.92.148.125:/var/www/xcmagg/
+    TODAY=$(date -u +%Y-%m-%d)
+    sed -i "s|<lastmod>.*</lastmod>|<lastmod>${TODAY}</lastmod>|g" public/sitemap.xml
 
-sed "s|fetch('data.jsonl')|fetch('https://racefeed.com.br/data.jsonl')|" public/index.html > /tmp/index_prod.html
-scp /tmp/index_prod.html root@164.92.148.125:/var/www/xcmagg/index.html
-rm /tmp/index_prod.html
+    scp public/* root@164.92.148.125:/var/www/xcmagg/
+
+    sed "s|fetch('data.jsonl')|fetch('https://racefeed.com.br/data.jsonl')|" public/index.html > /tmp/index_prod.html
+    scp /tmp/index_prod.html root@164.92.148.125:/var/www/xcmagg/index.html
+    rm /tmp/index_prod.html
+}
+
+if [[ "$1" == "--deploy" ]]; then
+    deploy
+else
+    uv run main.py
+    deploy
+fi
 
 
